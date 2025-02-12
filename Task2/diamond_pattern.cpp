979b5ec06c3840b2828c4fec2944a1ff0784a757
ShahdHafez251 <shahdhafez2532003@gmail.com>
2025-02-12 16:06:12 +0200

    Create diamond_pattern.cpp

---
+++ Task2/diamond_pattern.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    
+    cout << " enter number of rows :" ;
+    int row ;
+    cin >> row;
+
+    for (int i = 1; i <= row; i++) {
+        for (int j = 1; j <= row - i; j++)
+            cout << " ";
+        for (int j = 1; j <= 2 * i - 1; j++)
+            cout << "*";
+        cout << endl;
+    }
+    
+    for ( int i =row-1 ; i >= 1 ; i--){
+        for (int j = 1; j <= row - i; j++){
+            cout << " ";
+        }
+        for (int j = 1; j <= 2 * i - 1; j++)
+            cout << "*";
+        cout << endl;
+    }
+    system("pause");
+    
+    return 0;
+}
